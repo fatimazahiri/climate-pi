7b758f54130a94e61dc923c82cc47ecd4e74e0d7
DanDiCesare <daniel.dicesare22@gmail.com>
2020-03-22 20:18:58 -0400

    Added basic bme680 I/O

--- rpi/sensors/BME680.py
+++ rpi/sensors/BME680.py
@@ -1,1 +1,44 @@
 #!/usr/bin/python3
+#to install library, run 'sudo pip3 install adafruit-circuitpython-bme680'
+
+from busio import I2C
+import adafruit_bme680
+import time
+import board
+
+# Create library object using our Bus I2C port
+i2c = I2C(board.SCL, board.SDA)
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
+
+# change this to match the location's pressure (hPa) at sea level
+bme680.sea_level_pressure = 1032.0 #set for Hamilton, urban areas.
+
+#returns in Celsius
+def getTemperature():
+    return bme680.temperature
+
+#Returns in ohms
+def getGas():
+    return bme680.gas
+
+#Returns as a percentage
+def getHumidity():
+    return bme680.humidity
+
+#Returns in hPa
+def getPressure():
+    return bme680.pressure
+
+#Returns in meters above sea level
+def getAltitude():
+    return bme680.altitude
+
+def runForever():
+    while True:
+        print("\nTemperature: %0.1f C" % bme680.temperature)
+        print("Gas: %d ohm" % bme680.gas)
+        print("Humidity: %0.1f %%" % bme680.humidity)
+        print("Pressure: %0.3f hPa" % bme680.pressure)
+        print("Altitude = %0.2f meters" % bme680.altitude)
+
+        time.sleep(2)
