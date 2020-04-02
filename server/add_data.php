237157597c85ab39fa4456f85546f66146efedb3
Connor Czarnuch <connor.czarnuch@gmail.com>
2020-04-02 19:14:07 -0400

    add copyright statement

--- server/add_data.php
+++ server/add_data.php
@@ -12,6 +12,22 @@ function ExtendedAddslash(&$params)
 // Initialize ExtendedAddslash() function for every $_POST variable
 ExtendedAddslash($_POST);
 
+# Copyright (C) 2020  Connor Czarnuch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 $device_id =$_POST['device_id'];
 $location =$_POST['location'][0] .", ". $_POST['location'][1];
 $time =$_POST['time'];
