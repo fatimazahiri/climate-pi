204734e170d175cf7bb12c21ee7483642017c7b4
Connor Czarnuch <connor.czarnuch@gmail.com>
2020-04-05 13:31:06 -0400

    fix gas heater

--- rpi/sensors/BME680.py
+++ rpi/sensors/BME680.py
@@ -69,6 +69,10 @@ class BME680(BME680Data):
         self.set_temp_offset(0)
         self.get_sensor_data()
 
+        self.set_gas_heater_temperature(320)
+        self.set_gas_heater_duration(150)
+        self.select_gas_heater_profile(0)
+
     def _get_calibration_data(self):
         """Retrieve the sensor calibration data and store it in .calibration_data."""
         calibration = self._get_regs(BME_CONST.COEFF_ADDR1, BME_CONST.COEFF_ADDR1_LEN)
