b5ff1cdb510692b38b743b8851610cde8ae6a46c
Daniel Di Cesare <dan@pop-os.localdomain>
2020-05-04 15:32:35 -0400

    added style and db integration for more values

--- FormSite/handle.php
+++ FormSite/handle.php
@@ -1,6 +1,21 @@
 <?php
     $user_name = htmlspecialchars($_POST['user_name']);
     $user_email  = htmlspecialchars($_POST['user_email']);
+    $user_lat = htmlspecialchars($_POST['user_lat']);
+    $user_long = htmlspecialchars($_POST['user_long']);
+    $user_bme = FALSE;
+    $user_smp = FALSE;
+    $user_si1 = FALSE;
+
+    if ($_POST['A'] == 'BME680') {
+        $user_bme = TRUE;
+    }
+    if ($_POST['B'] == 'SMPWM01C') {
+        $user_smp = TRUE;
+    }
+    if ($_POST['C'] == 'Si1145') {
+        $user_si1 = TRUE;
+    }
 
     $db_host = 'localhost';
     $db_username = 'raspberrypi';
